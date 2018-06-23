411ec1f7ce7dec9c74992add5c7a75dcc59d2e77
Felipe Pereira <felipe@prodam.am.gov.br>
2018-06-23 17:50:52 -0400

    rename of route

--- src/main/java/com/vanhackthon/felipe/App.java
+++ src/main/java/com/vanhackthon/felipe/App.java
@@ -1,12 +1,12 @@
 package com.vanhackthon.felipe;
 
 import com.vanhackthon.felipe.route.AgendaRoute;
-import com.vanhackthon.felipe.route.HelloWorldRoute;
+import com.vanhackthon.felipe.route.HelloWorldTest;
 import com.vanhackthon.felipe.route.InfoRoute;
 
 public class App {
 	public static void main(String[] args) {
-		new HelloWorldRoute().init();
+		new HelloWorldTest().init();
 		new InfoRoute().init();
 		new AgendaRoute().init();
 	}
